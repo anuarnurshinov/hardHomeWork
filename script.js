a3e4fb8d159fbc718742dc8c8d9c8e24150079e9
Anuar Nurshinov <anuarnurshinov@gmaul.com>
2022-05-31 01:20:23 +0300

    Третий урок

--- script.js
+++
@@ -1,6 +0,0 @@
-let num = 266219;
-let newArr = num.toString().split('');
-console.log(newArr);
-let res = newArr.reduce((acc, rec) => acc * rec);
-console.log(res);
-console.log((res ** 3).toString().slice(0, 2))

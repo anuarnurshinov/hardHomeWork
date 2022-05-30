9c084d1f4069214c0060632e4ff38dea7baa608f
Anuar Nurshinov <anuarnurshinov@gmaul.com>
2022-05-31 01:20:23 +0300

    Третий урок завершен

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
-let num = 266219;
-let newArr = num.toString().split('');
-console.log(newArr);
-let res = newArr.reduce((acc, rec) => acc * rec);
-console.log(res);
-console.log((res ** 3).toString().slice(0, 2))
+let lang = "en"
+let week = [
+    [
+        "Понедельник", "Вторник", "Среда",
+        "Четверг", "Пятница", "Суббота",
+        "Воскресенье",
+    ],
+    [
+        "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday",
+        "Sunday"
+    ],
+];
+
+
+// Решение с помощью многомерного массива
+(lang == "ru") ?
+    week.splice(-1, 1) :
+    week.splice(0, 1);
+console.log(week);
+
+// Функции для switch и if
+function englishDaysPusher(arr) {
+    arr.length = 0;
+    arr.push(
+        "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday",
+        "Sunday")
+}
+function russianDaysPusher(arr) {
+    arr.length = 0;
+    arr.push(
+        "Понедельник", "Вторник", "Среда",
+        "Четверг", "Пятница", "Суббота",
+        "Воскресенье")
+}
+
+
+// Решение с помощью if
+if (lang == "ru") {
+    russianDaysPusher(week)
+    console.log(week)
+}
+else if (lang == "en") {
+    englishDaysPusher(week)
+    console.log(week)
+}
+
+
+// Решение с помощью switch
+switch (lang) {
+    case "ru":
+        russianDaysPusher(week)
+        console.log(week)
+        break;
+    case "en":
+        englishDaysPusher(week)
+        console.log(week)
+        break;
+}
+
+
